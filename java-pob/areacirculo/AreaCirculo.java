8f9435243d540f52d22626dd281daaa231723d58
DanielBrascherFaeterj <daniel.24104708360030@faeterj-rio.edu.br>
2025-02-26 10:28:17 -0300

    Create AreaCirculo.java

---
+++ java-pob/areacirculo/AreaCirculo.java
@@ -0,0 +1,21 @@
+package areacirculo;
+
+import java.util.Scanner;
+
+public class AreaCirculo {
+    
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o valor do raio: ");
+        double raio = sc.nextDouble();
+        sc.close();
+
+        double area = Math.PI * Math.pow(raio, 2);
+
+        System.out.printf("A area do circulo com raio %.2f é %.2f\n", raio, area);
+    }
+    
+    
+}
