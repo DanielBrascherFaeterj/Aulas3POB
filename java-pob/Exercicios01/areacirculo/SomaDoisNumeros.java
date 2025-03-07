e74beac12889501495206f034c0f1e9c51a7363b
DanielBrascher <dbrascher@gmail.com>
2025-03-06 23:34:11 -0300

    exercicio1

---
+++ java-pob/Exercicios01/areacirculo/SomaDoisNumeros.java
@@ -0,0 +1,19 @@
+package areacirculo;
+
+import java.util.Scanner;
+
+public class SomaDoisNumeros {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro numero: ");
+        int n1 = sc.nextInt();
+
+        System.out.print("Digite o segundo numero: ");
+        int n2 = sc.nextInt();
+        sc.close();
+
+        System.out.printf("Resultado: %d", n1+n2);
+    }
+}
