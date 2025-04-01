cdad45054327493780a48e890af9dde07414e5ef
Daniel do Amaral Brascher <alun24104708360030@h3-07.faculdade.edu>
2025-04-01 10:59:20 -0300

    lista4 ex2

---
+++ java-pob/Exercicios04/ex02/MediaNotas.java
@@ -0,0 +1,28 @@
+package ex02;
+
+import java.util.Scanner;
+
+public class MediaNotas {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        float array[] = new float[4];
+        float tot = 0;
+
+        for(int i = 0; i < 4; i++){
+            System.out.print("digite o valor da nota: ");
+            array[i] = sc.nextFloat();
+            tot += array[i];
+        }
+        sc.close();
+        
+        if(tot/4 >= 7){
+            System.out.println("aprovado!");
+        }
+        else{
+            System.out.println("reprovado!");
+        }
+
+    }
+}
